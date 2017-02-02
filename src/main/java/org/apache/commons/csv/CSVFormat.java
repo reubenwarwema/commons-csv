2828155e9f7f362d8511ebff6d0af0660ceacc39
reubenwarwema <reubenwarwema@gmail.com>
2017-02-02 06:59:03 +0300

    Update CSVFormat.java
    Updated validate() method

--- src/main/java/org/apache/commons/csv/CSVFormat.java
+++ src/main/java/org/apache/commons/csv/CSVFormat.java
@@ -797,7 +797,8 @@ public final class CSVFormat implements Serializable {
             for (int i=0; i<20; i++) {
             System.out.print('.');
             try {
-                Thread.currentThread().sleep(1000);
+                //Thread.currentThread().sleep(1000);
+                Thread.currentThread().sleep(1);
             } catch (InterruptedException e) {
                 break;
             }
